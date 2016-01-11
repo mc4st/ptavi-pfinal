54a27c33a0ccc5e644cfe1f88df9a5f13ad18ae1
mc4st <C4st3j0n>
2016-01-11 12:13:26 +0100

    modificando

--- uaserver.py
+++ uaserver.py
@@ -8,6 +8,50 @@ import socketserver
 import sys
 import os
 
+if len(sys.argv) == 2:
+    configXML = sys.argv[1]
+else:
+    sys.exit('Usage: python3 uaserver.py config')
+
+class ExtraerXML (ContentHandler):
+    def __init__(self):
+        self.taglist = []
+        self.tags = [
+            'account', 'uaserver', 'rtpaudio', 'regproxy', 'log', 'audio']
+        self.diccattributes = {
+            'account': ['username', 'passwd'],
+            'uaserver': ['ip', 'puerto'],
+            'rtpaudio': ['puerto'],
+            'regproxy': ['ip', 'puerto'],
+            'log': ['path'],
+            'audio': ['path']}
+
+    def startElement(self, tag, attrs):
+        dicc = {}
+        # si existe la etiqueta en mi lista de etiquetas.
+        if tag in self.tags:
+            # recorro todos los atributos, guardo en diccionario.
+            for attribute in self.diccattributes[tag]:
+                dicc[attribute] = attrs.get(attribute, "")
+            # voy encadenando la lista, guardo a continuación sin sustituir
+            # lo que tiene dentro.
+            self.taglist.append([tag, dicc])
+
+    def get_tags(self):
+        return self.taglist
+
+parser = make_parser()
+XMLHandler = ExtraerXML()
+parser.setContentHandler(XMLHandler)
+parser.parse(open(configXML))
+listXML = XMLHandler.get_tags()
+user = listXML[0][1]['username']
+ua_port = listXML[1][1]['puerto']
+ua_ip = listXML[1][1]['ip']
+audio_port = listXML[2][1]['puerto']
+proxy_ip = listXML[3][1]['ip']
+proxy_port = int(listXML[3][1]['puerto'])
+audio = int(listXML[5][1]['path'])
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -15,9 +59,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        IP = self.client_address[0]
-        PORT = self.client_address[1]
-        fich_audio = sys.argv[3]
         while 1:
             line_bytes = self.rfile.read()
             line = line_bytes.decode('utf-8')
@@ -43,8 +84,6 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                                  b"\r\n")
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    PORT = int(sys.argv[2])
-    IP = sys.argv[1]
-    serv = socketserver.UDPServer((IP, PORT), EchoHandler)
+    serv = socketserver.UDPServer((ua_ip, ua_port), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
